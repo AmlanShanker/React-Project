0921ee0d013175523e8d38c3ca7fbae5924a3c22
Amlan Shanker <amlanshanker2005@gmail.com>
2026-07-25 11:09:16 +0530

    vercel backend setup

--- server/server.js
+++ server/server.js
@@ -12,8 +12,12 @@ app.use(express.json());
 app.use("/movies", movieRoutes);
 app.use("/bookings", bookingRoutes);
 
-const PORT = 3000;
+module.exports = app;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
